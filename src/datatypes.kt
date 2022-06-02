e696299be170ff4314daebf8aca48278fa70d7f1
Kayuemkhan <abdulkayuem007@gmail.com>
2022-06-02 16:21:01 +0600

    datatypes added

---
+++ src/datatypes.kt
@@ -0,0 +1,36 @@
+fun main() {
+    // 4 types of data types - > Integer, Floating , Boolean, Character.
+    // Integer data types -> byte, short, int, long
+    val intA  = 23;
+    println(intA)
+
+    val myLong = 23L
+    println(myLong)
+
+    // floating, double ->
+    val myFloating = Float.MAX_VALUE
+    println(myFloating)
+
+    val d1: Double = Double.MIN_VALUE
+    val d2: Double = Double.MAX_VALUE
+    println("Smallest Double value: $d1")
+    println("Largest Double value: $d2")
+
+    // Boolean types:
+    val myBool = true
+    val myBool2 = false;
+    println(myBool)
+    println(myBool2)
+
+    // Characters
+    val myChar = 'a'
+    println(myChar)
+
+
+
+
+
+
+
+
+}
