53851367df94f96a88a75c6b9bd9208e2021e20e
Kayuemkhan <abdulkayuem007@gmail.com>
2022-06-02 15:53:35 +0600

    helloworld in kotlin added

---
+++ src/helloworld.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    var string: String? = null
+
+    if(string != null) {               // smart cast
+        println("Hello sadia")
+        print(string.length)
+    }
+
+}
