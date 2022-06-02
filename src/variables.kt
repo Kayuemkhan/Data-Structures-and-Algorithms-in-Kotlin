9ed40352730888559e594974d3948738e6d94348
Kayuemkhan <abdulkayuem007@gmail.com>
2022-06-02 17:52:59 +0600

    datatypes added

---
+++ src/variables.kt
@@ -0,0 +1,13 @@
+fun main() {
+
+    var rollno = 55
+    val name = "Abdul"
+    println(rollno)
+    println(name)
+     // two types of variable
+    //  Immutable using val keyword
+    //  Mutable using var keyword
+    //  Immutable is also called read-only variables.
+    //  Hence, we can not change the value of the variable declared using val keyword.
+    // val are not final
+}
